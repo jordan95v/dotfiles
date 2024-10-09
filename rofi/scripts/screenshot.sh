7881612b9f71ce79993051e60eb58d091d35fc80
jordan95v <90615280+jordan95v@users.noreply.github.com>
2024-10-09 10:47:37 +0200

    Fixed missing space in 'screenshot.sh'.

--- rofi/scripts/screenshot.sh
+++ rofi/scripts/screenshot.sh
@@ -4,7 +4,7 @@ filename=$(date +'%s_grim.png')
 all="󰹑  -> All outputs"
 all_clip="󰆏  -> Copy all outputs to clipboard"
 select="  -> Select region"
-select_clip=" -> Copy selected region to clipboard"
+select_clip="  -> Copy selected region to clipboard"
 input=$(printf "%s\n%s\n%s\n%s\n" "$all" "$all_clip" "$select" "$select_clip" | rofi -dmenu -p " 󰤨  Screenshot " -lines 4) || exit 1
 sleep 1 # to close rofi menu
 
